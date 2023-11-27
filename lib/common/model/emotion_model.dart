bde5c5f165d8787b4d831e56868f6fcc8a4cd70e
seongmin221 <seongmin221@naver.com>
2023-11-27 21:09:32 +0900

    emotion models added

--- lib/common/model/emotion_model.dart
+++ lib/common/model/emotion_model.dart
@@ -1,21 +1,50 @@
-enum EmotionCatagory
+import 'package:emotion_diary/common/utils/emojis.dart';
 
-class EmotionCategoryModel {
-  String? category;
-  List<EmotionModel>? words;
+enum EmotionCategory {
+  bigSmile("웃김"),
+  smile("기쁨"),
+  angry("화남"),
+  surprised("놀람"),
+  sad("슬픔");
 
-  init(String category, List<EmotionModel> words) {
-    this.category = category;
-    this.words = words;
+  final String korean;
+  const EmotionCategory(this.korean);
+}
+
+extension EmotionCategoryImagePath on EmotionCategory {
+  String imagePath() {
+    switch (this) {
+      case EmotionCategory.bigSmile: return Emojis.bigSmile;
+      case EmotionCategory.smile: return Emojis.smile;
+      case EmotionCategory.angry: return Emojis.angry;
+      case EmotionCategory.surprised: return Emojis.surprised;
+      case EmotionCategory.sad: return Emojis.sad;
+    }
   }
 }
 
+
+class EmotionCategoryModel {
+  EmotionCategoryModel({required this.category, required this.words});
+
+  EmotionCategory? category;
+  List<EmotionModel>? words;
+}
+
+extension EmotionCategoryList on EmotionCategoryModel {
+  static List<EmotionCategoryModel> categories = [
+    EmotionCategoryModel(category: EmotionCategory.bigSmile, words: []),
+    EmotionCategoryModel(category: EmotionCategory.smile, words: []),
+    EmotionCategoryModel(category: EmotionCategory.angry, words: []),
+    EmotionCategoryModel(category: EmotionCategory.surprised, words: []),
+    EmotionCategoryModel(category: EmotionCategory.sad, words: []),
+  ];
+}
+
+
 class EmotionModel {
+  EmotionModel({required this.word, required this.definition});
+
   String? word;
   String? definition;
-
-  init(String word, String definition) {
-    this.word = word;
-    this.definition = definition;
-  }
 }
